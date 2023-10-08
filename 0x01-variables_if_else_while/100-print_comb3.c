af0863f706aa6632f648920c3ba170f324efc938
yasseramr-1 <yasseramr300@gmail.com>
2023-10-08 10:24:55 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+/**
+* main - This code prints all possible
+* different combinations of two digits
+*
+* Return: Always 0 (SUCCESS)
+*/
+int main(void)
+{
+int i,j=1,b=1;
+for(i = 0 ; i < 9 ; i++)
+{
+for( ; j < 10 ; j++)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar(', ');
+}
+b++;
+j=b;
+}
+putchar('\n');
+return (0);
+}
