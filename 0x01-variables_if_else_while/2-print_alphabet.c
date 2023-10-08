039f3607e6ae2bbb4b870c1457c99c048d517ff9
yasseramr-1 <yasseramr300@gmail.com>
2023-10-08 09:15:58 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 /*
-* main- This code prints the alphabet in lowercase
+* main - This code prints the alphabet in lowercase
 *
 * Return: Always 0 (SUCCESS)
 */
