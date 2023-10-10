6510dbd41d7924af259860d647844b172655300d
yasseramr-1 <yasseramr300@gmail.com>
2023-10-10 14:03:57 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main-prints putchar 
- * Return: Always 0 (Success)
- */
+* main-prints putchar 
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 printf("_putchar\n");
