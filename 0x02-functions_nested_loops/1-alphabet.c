6393f11697bf13e5a9778b477665e03ccafdb113
yasseramr-1 <yasseramr300@gmail.com>
2023-10-10 14:19:57 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_alphabet - prints the alphabet in lowercase form
+ */
+void print_alphabet(void)
+{
+char c = 'a';
+for (int i = 0; i < 26; i++)
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+}
