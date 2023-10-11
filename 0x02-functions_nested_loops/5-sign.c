c6ab2bfad60beb1ab5f2453a39429a6ac15a2226
yasseramr-1 <yasseramr300@gmail.com>
2023-10-10 17:54:06 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,19 @@
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        putchar('+');
-        return 1;
-    }
-    else if (n < 0)
-    {
-        putchar('-');
-        return -1;
-    }
-    else
-    {
-        putchar('0');
-        return 0;
-    }
+if (n > 0)
+{
+putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+putchar(45);
+return (-1);
+}
+else
+{
+putchar(48);
+return (0);
+}
 }
