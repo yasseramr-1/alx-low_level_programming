25e854130b70fd18f4f8df3df9c294a22f377900
yasseramr-1 <yasseramr300@gmail.com>
2023-10-10 17:25:37 -0700

    d

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * main - entry point of the program
- * Return: Always 0 (Success)
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-print_alphabet();
-return (0);
+    print_alphabet();
+    return (0);
 }
