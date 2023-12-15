524445e1cdd297ef17bfdcca7569fb17eebc4b37
yasser <yasseramr300@gmail.com>
2023-12-15 11:51:14 -0800

    s

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,8 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdio.h>
+#ifndef LISTS
+#define LISTS
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - singly linked list
@@ -10,8 +10,8 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
-*/
+ * for Holberton project
+ */
 typedef struct listint_s
 {
 	int n;
@@ -28,5 +28,9 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-
-#endif /* LISTS_H */
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
+#endif
